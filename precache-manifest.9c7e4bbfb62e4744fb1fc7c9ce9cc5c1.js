b548d2fc951ea8248c187c6b5f2826ce0bbdca3f
viveksai26 <viveksai26@gmail.com>
2021-07-29 15:58:23 +0000

    Deploying to gh-pages from @ viveksai26/RNapp@303164868b3c523756c698a13d8819ae0e66c4ae 🚀

---
+++ precache-manifest.9c7e4bbfb62e4744fb1fc7c9ce9cc5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220a07349b9c5d59b829226b75fc01ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6b07015b56953c3fba",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "313b2668f639e5fae2f3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac6b07015b56953c3fba",
+    "url": "/static/js/2.5597bbe2.chunk.js"
+  },
+  {
+    "revision": "313b2668f639e5fae2f3",
+    "url": "/static/js/main.f9867347.chunk.js"
+  },
+  {
+    "revision": "7b3f0a0ae53e36a7d1a3",
+    "url": "/static/js/runtime-main.7b2fe198.js"
+  }
+]);
